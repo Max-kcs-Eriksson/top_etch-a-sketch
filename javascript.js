ee141f09b6a6b00d7a4241ea8736f32b9b293727
Max <max.kcs.eriksson@hotmail.com>
2022-07-10 16:14:37 +0200

    Create .js file with pseudocode

---
+++ javascript.js
@@ -0,0 +1,12 @@
+/* User press 'SET X&Y' */
+    /* Prompt to set X & Y axis */
+        /* X axis = Y axis */
+    /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
+        /* Set style property: width & height */
+            /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
+        /* Boxes wrap to fit '.drawing-pad' */
+        /* Add hover event listener for each box */
+            /* Add color specific class name on hover */
+
+/* Erase button */
+    /* Removes color specific class names of all boxes */
