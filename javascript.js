c1bc9bfafaeb05d9c164abe8889a1f9ded4d0bac
Max <max.kcs.eriksson@hotmail.com>
2022-07-11 00:17:09 +0200

    Set click event listener on the "Set X&Y" button with an arrow function
    The arrow function assigns a number to xAxis via a prompt.
    All previously written code to append divs with class names etc have been moved inside the arrow function. This is to fix issues with scope of variables, and now the boxes are appended after a number i assigned to xAxis via above mentioned promt.

--- javascript.js
+++ javascript.js
@@ -1,43 +1,49 @@
 const DRAW_PAD_WIDTH_PX = 800; // Height = Width
 const drawPad = document.querySelector('.drawing-pad');
 
-/* User press 'SET X&Y' */
-    /* Prompt to set number of boxes on X & Y axis */
-        /* X axis = Y axis */
-    // FOR TESTING ONLY
-const xAxis = 2;
-    // const xAxis = 16;
-const yAxis = xAxis;
-    //    TO HERE
-const drawBoxTotal = xAxis * yAxis;
-    // console.log('Total num boxes ' + drawBoxTotal);
-    /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
-for (let i = 0; i < drawBoxTotal; i++) {
-    const drawBox = document.createElement('div');
-    drawBox.classList.add('draw-box');
-    drawPad.appendChild(drawBox);
-};
-            /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
-const drawBoxWidth = DRAW_PAD_WIDTH_PX / xAxis;
-            // console.log('X Axis num of Boxes: ' + xAxis);
-            // console.log('Y Axis num of Boxes: ' + yAxis);
-            // console.log('Box Width: ' + drawBoxWidth);
-            /* Boxes wrap to fit '.drawing-pad' */
-            
-const drawBoxes = document.querySelectorAll('.draw-box');
-      
-drawBoxes.forEach((div => {
-    /* Set style property: width & height */
-    div.style.width = `${drawBoxWidth}px`;
-    div.style.height = `${drawBoxWidth}px`;
-    /* Add mouseenter event listener for each box */
-    div.addEventListener('mouseenter', paintBox)
-}));
 
-            /* Add color specific class name on hover */
+/* User press 'SET X&Y' */
+const setAxisButton = document.querySelector('#set-x-y');
+setAxisButton.addEventListener('click', () => {
+    /* Prompt to set number of boxes on X & Y axis */
+    let xAxis = Number(prompt('Enter desired number of boxes for X& Y axis', ));
+    let yAxis = xAxis;
+    
+    const drawBoxTotal = xAxis * yAxis;
+        console.log('Total num boxes ' + drawBoxTotal);
+
+    /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
+    for (let i = 0; i < drawBoxTotal; i++) {
+        const drawBox = document.createElement('div');
+        drawBox.classList.add('draw-box');
+        drawPad.appendChild(drawBox);
+    };
+
+    /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
+    const drawBoxWidth = DRAW_PAD_WIDTH_PX / xAxis;
+        console.log('X Axis num of Boxes: ' + xAxis);
+        console.log('Y Axis num of Boxes: ' + yAxis);
+        console.log('Box Width: ' + drawBoxWidth);
+
+    const drawBoxes = document.querySelectorAll('.draw-box');
+    drawBoxes.forEach((div => {
+        /* Set style property: width & height */
+        div.style.width = `${drawBoxWidth}px`;
+        div.style.height = `${drawBoxWidth}px`;
+        /* Add mouseenter event listener for each box */
+        div.addEventListener('mouseenter', paintBox)
+    }));
+
+
+
+
+});
+
 
 /* Erase button */
     /* Removes color specific class names of all boxes */
+
+
 function paintBox() {
     this.classList.add('painted');
 };
