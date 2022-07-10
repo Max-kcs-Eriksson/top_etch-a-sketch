e51e5b526183624d13beb91893e4aee187735ef9
Max <max.kcs.eriksson@hotmail.com>
2022-07-10 21:04:42 +0200

    Add fundamental variables, functions, and event listeners
    The added variables are used to calculate width and height of boxes to be appended to .drawing-pad and to dictate how many boxes are to be appended on the X and Y axis.
    A single div with class of "draw-box" is appended for testing reasons, and a mouseenter event listener calling paintBox() is added to above mentioned div.
    The function paintBox() add "painted" to the class list of the DOM node that triggered the function to be called via above described event listener.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
+const DRAW_PAD_WIDTH_PX = 800; // Height = Width
+const drawPad = document.querySelector('.drawing-pad');
+
 /* User press 'SET X&Y' */
-    /* Prompt to set X & Y axis */
+    /* Prompt to set number of boxes on X & Y axis */
         /* X axis = Y axis */
+    // FOR TESTING ONLY
+    const xAxis = 2;
+    // const xAxis = 16;
+    const yAxis = xAxis;
+    //    TO HERE
     /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
+        // FOR TESTING ONLY
+        const drawBox = document.createElement('div');
+        drawBox.classList.add('draw-box');
+        drawPad.appendChild(drawBox);
+        //    TO HERE
         /* Set style property: width & height */
             /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
+            const boxWidth = DRAW_PAD_WIDTH_PX / xAxis;
+            console.log('X Axis num of Boxes: ' + xAxis);
+            console.log('Y Axis num of Boxes: ' + yAxis);
+            console.log('Box Width: ' + boxWidth);
+            drawBox.style.width = `${boxWidth}px`; // FIX
+            drawBox.style.height = `${boxWidth}px`; // FIX
         /* Boxes wrap to fit '.drawing-pad' */
         /* Add hover event listener for each box */
+        const drawBoxes = document.querySelectorAll('.draw-box');
+        drawBoxes.forEach((div => {
+            div.addEventListener('mouseenter', paintBox)
+        }));
+
             /* Add color specific class name on hover */
 
 /* Erase button */
     /* Removes color specific class names of all boxes */
+function paintBox() {
+    this.classList.add('painted');
+};
