3a1c81cd2c49083fb1826c742236586961711bbf
Max <max.kcs.eriksson@hotmail.com>
2022-07-10 22:59:58 +0200

    Add for loop to create, give class name, and append div elements to drawPad

--- javascript.js
+++ javascript.js
@@ -5,30 +5,34 @@ const drawPad = document.querySelector('.drawing-pad');
     /* Prompt to set number of boxes on X & Y axis */
         /* X axis = Y axis */
     // FOR TESTING ONLY
-    const xAxis = 2;
+const xAxis = 2;
     // const xAxis = 16;
-    const yAxis = xAxis;
+const yAxis = xAxis;
     //    TO HERE
+const drawBoxTotal = xAxis * yAxis;
+    // console.log('Total num boxes ' + drawBoxTotal);
     /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
-        // FOR TESTING ONLY
-        const drawBox = document.createElement('div');
-        drawBox.classList.add('draw-box');
-        drawPad.appendChild(drawBox);
-        //    TO HERE
-        /* Set style property: width & height */
+for (let i = 0; i < drawBoxTotal; i++) {
+    const drawBox = document.createElement('div');
+    drawBox.classList.add('draw-box');
+    drawPad.appendChild(drawBox);
+};
             /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
-            const boxWidth = DRAW_PAD_WIDTH_PX / xAxis;
-            console.log('X Axis num of Boxes: ' + xAxis);
-            console.log('Y Axis num of Boxes: ' + yAxis);
-            console.log('Box Width: ' + boxWidth);
-            drawBox.style.width = `${boxWidth}px`; // FIX
-            drawBox.style.height = `${boxWidth}px`; // FIX
-        /* Boxes wrap to fit '.drawing-pad' */
-        /* Add hover event listener for each box */
-        const drawBoxes = document.querySelectorAll('.draw-box');
-        drawBoxes.forEach((div => {
-            div.addEventListener('mouseenter', paintBox)
-        }));
+const drawBoxWidth = DRAW_PAD_WIDTH_PX / xAxis;
+            // console.log('X Axis num of Boxes: ' + xAxis);
+            // console.log('Y Axis num of Boxes: ' + yAxis);
+            // console.log('Box Width: ' + drawBoxWidth);
+            /* Boxes wrap to fit '.drawing-pad' */
+            
+const drawBoxes = document.querySelectorAll('.draw-box');
+      
+drawBoxes.forEach((div => {
+    /* Set style property: width & height */
+    div.style.width = `${drawBoxWidth}px`;
+    div.style.height = `${drawBoxWidth}px`;
+    /* Add mouseenter event listener for each box */
+    div.addEventListener('mouseenter', paintBox)
+}));
 
             /* Add color specific class name on hover */
 
