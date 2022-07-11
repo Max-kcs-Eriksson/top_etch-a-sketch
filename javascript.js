a2c2b4e8f4db6f51ff187606cfb6b5b24c0e0e63
Max <max.kcs.eriksson@hotmail.com>
2022-07-11 16:48:32 +0200

    Add if statement to arrow function called by click event listener on setAxisButton to remove old box grid befor appending a new one according to new axisSliderValue

--- javascript.js
+++ javascript.js
@@ -5,47 +5,97 @@ console.log('drawPadWidthPX: ' + drawPadWidthPX);
 console.log('drawPadWidthNum: ' + drawPadWidthNum);
 
 
+
 /* User selects X-& Y-axis */
 const setAxisSlider = document.querySelector('#set-x-y');
 const axisSliderValue = document.querySelector('#display-x-y');
-
+// Displays value on range input
 setAxisSlider.oninput = () => {
     axisSliderValue.textContent = setAxisSlider.value;
 }
 /* Confirms axisSliderValue and created grid of drawBox with set X-& Y-axis */
 const setAxisButton = document.querySelector('#confirm-x-y');
 setAxisButton.addEventListener('click', () => {
-    const xAxis = setAxisSlider.value;
-    const yAxis = xAxis;
-    
-    const drawBoxTotal = xAxis * yAxis;
-        console.log('Total num boxes ' + drawBoxTotal);
+    // Old draw box nodes
+    const oldDrawBoxes = document.querySelectorAll('.draw-box');
+    const oldDrawBox = document.querySelector('.draw-box');
+    // TODO
+    // Remove old grid of boxes
+    // const oldDrawBoxes = document.querySelectorAll('.draw-box');
+    // const oldDrawBox = document.querySelector('.draw-box');
 
-    /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
-    for (let i = 0; i < drawBoxTotal; i++) {
-        const drawBox = document.createElement('div');
-        drawBox.classList.add('draw-box');
-        drawPad.appendChild(drawBox);
-    };
+        
+    if (!oldDrawBox) {
+        const xAxis = setAxisSlider.value;
+        const yAxis = xAxis;
+        
+        const drawBoxTotal = xAxis * yAxis;
+            console.log('Total num boxes ' + drawBoxTotal);
 
-    /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
-    const drawBoxWidth = drawPadWidthNum / xAxis;
-        console.log('X Axis num of Boxes: ' + xAxis);
-        console.log('Y Axis num of Boxes: ' + yAxis);
-        console.log('Box Width: ' + drawBoxWidth);
+        /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
+        for (let i = 0; i < drawBoxTotal; i++) {
+            const drawBox = document.createElement('div');
+            drawBox.classList.add('draw-box');
+            drawPad.appendChild(drawBox);
+        };
 
-    const drawBoxes = document.querySelectorAll('.draw-box');
-    drawBoxes.forEach((div => {
-        /* Set style property: width & height */
-        div.style.width = `${drawBoxWidth}px`;
-        div.style.height = `${drawBoxWidth}px`;
-        /* Add mouseenter event listener for each box */
-        div.addEventListener('mouseenter', paintBox)
-    }));
+        /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
+        const drawBoxWidth = drawPadWidthNum / xAxis;
+            console.log('X Axis num of Boxes: ' + xAxis);
+            console.log('Y Axis num of Boxes: ' + yAxis);
+            console.log('Box Width: ' + drawBoxWidth + 'px');
+
+        const drawBoxes = document.querySelectorAll('.draw-box');
+        drawBoxes.forEach((div => {
+            /* Set style property: width & height */
+            div.style.width = `${drawBoxWidth}px`;
+            div.style.height = `${drawBoxWidth}px`;
+            /* Add mouseenter event listener for each box */
+            div.addEventListener('mouseenter', paintBox);
+        }));
+    } else {
+        for (let i = 0; i < oldDrawBoxes.length; i++) {
+            drawPad.removeChild(drawPad.lastChild);
+        }
+
+        /* SAME CODE AS if (!oldDrawBox) */
+        const xAxis = setAxisSlider.value;
+        const yAxis = xAxis;
+        
+        const drawBoxTotal = xAxis * yAxis;
+            console.log('Total num boxes ' + drawBoxTotal);
+
+        /* Loop creates and appends (xAxis * yAxis) boxes in DOM node ('.drawing-pad')*/
+        for (let i = 0; i < drawBoxTotal; i++) {
+            const drawBox = document.createElement('div');
+            drawBox.classList.add('draw-box');
+            drawPad.appendChild(drawBox);
+        };
+
+        /* Box width and height are calculated based on '.drawing-pad' size and number of boxes */
+        const drawBoxWidth = drawPadWidthNum / xAxis;
+            console.log('X Axis num of Boxes: ' + xAxis);
+            console.log('Y Axis num of Boxes: ' + yAxis);
+            console.log('Box Width: ' + drawBoxWidth + 'px');
+
+        const drawBoxes = document.querySelectorAll('.draw-box');
+        drawBoxes.forEach((div => {
+            /* Set style property: width & height */
+            div.style.width = `${drawBoxWidth}px`;
+            div.style.height = `${drawBoxWidth}px`;
+            /* Add mouseenter event listener for each box */
+            div.addEventListener('mouseenter', paintBox);
+        }));
+
+    }
 
 
 
-
+    console.log('***TESTING***');
+    console.log('oldDrawBox exist: ' + oldDrawBox);
+    console.log('oldDrawBoxes exist: ' + oldDrawBoxes);
+    console.log('oldDrawBoxes DOM list length: ' + oldDrawBoxes.length);
+    console.log('***TESTING***');
 });
 
 
